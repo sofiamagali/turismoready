262dd148b64aaf06d7c90c6daece67dbf437eae4
Sofia Fuhrmann <sofia.fuhrmann@gmail.com>
2026-08-24 20:03:21 -0300

    feat: initialize Rails app with user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
+  validates :phone,
+            format: { with: /\A[\d\s()+.-]+\z/, message: "solo puede incluir números, espacios, +, paréntesis, puntos y guiones" },
+            allow_blank: true
+end
